34dd0d5bbaa3a1353deffa64dc6ddabef519b2f8
Ketkukelmi <t5mari00@students.oamk.fi>
2017-10-03 10:13:12 +0300

    Basic structure

---
+++ AwesomeComicViewer/app/src/main/java/rikumarttinen/awesomecomicviewer/ComicViewerClass.java
@@ -0,0 +1,10 @@
+package rikumarttinen.awesomecomicviewer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by martt_000 on 3.10.2017.
+ */
+
+public class ComicViewerClass extends AppCompatActivity {
+}
