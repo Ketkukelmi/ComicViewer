8622eeb6f631f002c5939f0e1f87def8571bdf5f
Jere Luomajoki <jpluomajoki@gmail.com>
2017-10-10 08:28:22 +0300

    added picasso

--- AwesomeComicViewer/app/src/main/java/rikumarttinen/awesomecomicviewer/ComicViewerClass.java
+++ AwesomeComicViewer/app/src/main/java/rikumarttinen/awesomecomicviewer/ComicViewerClass.java
@@ -1,10 +1,24 @@
 package rikumarttinen.awesomecomicviewer;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * Created by martt_000 on 3.10.2017.
  */
 
 public class ComicViewerClass extends AppCompatActivity {
+    ImageView comicView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        comicView = (ImageView) findViewById(R.id.comicView);
+
+        Picasso.with(this)
+                .load("https://hs.mediadelivery.fi/img/1920/23c29b6be1c2496a953911b4531bd1ff.jpg");
+    }
 }
